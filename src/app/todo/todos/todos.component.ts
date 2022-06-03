d7acbed4fe1ad32a30ae1448920a1760c73277e4
Danylo Vovchok <vovchok.danya01@gmail.com>
2022-06-03 23:02:31 +0200

    create todos page and routing

---
+++ src/app/todo/todos/todos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../shared/models/todo.model';
+import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss'],
+})
+export class TodosComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+
+
+}
