959f4867e0832230be981cc620cd3c9a64597879
Danylo Vovchok <vovchok.danya01@gmail.com>
2022-06-04 14:44:58 +0200

    filter by date

--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../shared/models/todo.model';
 import { TodoService } from 'src/app/shared/services/todo.service';
+import * as moment from 'moment';
 
 
 @Component({
@@ -12,6 +13,7 @@ export class TodosComponent implements OnInit {
   
   public todos: Todo[];
   public sort: string = 'newToOld';
+  public filterDate: Date;
 
   constructor(
     private todoService: TodoService,
@@ -37,6 +39,16 @@ export class TodosComponent implements OnInit {
         this.todos = Object.values(data);
       }
     });
+}
+
+public filterByDate(){
+  if (this.filterDate){
+    this.todoService.getTodos().subscribe(data=> {
+      this.todos = Object.values(data).filter(item => {
+        return moment(item.timestamp).format("DD-MM-YY").toString() == moment(this.filterDate).format("DD-MM-YY").toString()
+      });
+    });
+}
 }
 
 
