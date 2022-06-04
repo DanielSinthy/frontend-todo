899522bcf8336f8f132f07fc39263ed0cf4a6eed
Danylo Vovchok <vovchok.danya01@gmail.com>
2022-06-04 14:20:09 +0200

    show date on item

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Todo } from '../../shared/models/todo.model';
 import { TodoService } from 'src/app/shared/services/todo.service';
+import * as moment from 'moment';
 
 
 @Component({
@@ -16,6 +17,10 @@ export class TodoItemComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  get timestamp(){
+    return moment(this.todoItem.timestamp).format("DD-MM-YYYY")
   }
 
 
